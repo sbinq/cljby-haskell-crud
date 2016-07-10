b99cbf88a8583118035bcc479d97f179ea0a09cb
Stas Binko <stas.binko@gmail.com>
2016-07-10 05:09:27 +0300

    Add spock dependency

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Lib
+import Web.Spock.Safe (runSpock, spockT, get, post, (<//>), text, var)
+
 
 main :: IO ()
 main = do
-  someFunc
+  runSpock 8080 $ spockT id $ do
+
+    get ("/cat" <//> var ) $ \(catId :: Int) -> do
+      text $ "hello"
+
+    post "/cat" $ do
+      undefined
+
+    get ("/dog" <//> var) $ \(dogId :: Int) -> do
+      undefined
+
+    post "/dog" $ do
+      undefined
+
