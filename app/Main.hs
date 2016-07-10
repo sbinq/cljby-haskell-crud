56cebb91f2e6b6ae3875d74e96849cd86ec7ddd0
Stas Binko <stas.binko@gmail.com>
2016-07-10 07:02:51 +0300

    Add sqlite dependencies

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,15 @@
 
 module Main where
 
+import Control.Monad.Logger (runStdoutLoggingT)
+import Database.Persist.Sqlite (SqlBackend, createSqlitePool)
+import Data.Pool (Pool)
 import Web.Spock.Safe (runSpock, spockT, get, post, (<//>), text, var)
-
 
 main :: IO ()
 main = do
+  (pool :: Pool SqlBackend) <- runStdoutLoggingT $ createSqlitePool "/tmp/sample.db" 4
+
   runSpock 8080 $ spockT id $ do
 
     get ("/cat" <//> var ) $ \(catId :: Int) -> do
