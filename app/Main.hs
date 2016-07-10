93dd425e954a7e8ae03ff3cb2864aa5b94ba7003
Stas Binko <stas.binko@gmail.com>
2016-07-10 23:40:50 +0300

    Implement update or insert api

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -5,14 +6,16 @@ module Main where
 
 import           Control.Monad.Logger      (runStdoutLoggingT)
 import           Control.Monad.Trans.Class (lift)
-import           Data.Aeson                (ToJSON)
+import           Data.Aeson                (ToJSON, object, (.=))
 import           Data.Int                  (Int64)
-import           Database.Persist          (Entity)
+import           Database.Persist          (Entity, Key)
 import           Database.Persist.Sqlite   (createSqlitePool)
-import           Network.HTTP.Types        (status404)
-import           Web.Spock.Safe            (ActionT, get, json, post, runSpock, setStatus, spockT, var, (<//>))
+import           Network.HTTP.Types        (status400, status404)
+import           Web.Spock.Safe            (ActionT, get, json, jsonBody', post, runSpock, setStatus, spockT, var,
+                                            (<//>))
 
-import           Lib                       (AppM, Cat, Dog, encodeEntity, findById, initializeDatabase, runAppM)
+import           Lib                       (AppM, Cat, Dog, UpdateOrInsert, encodeEntity, findById, initializeDatabase,
+                                            runAppM, updateOrInsert)
 
 
 main :: IO ()
@@ -26,13 +29,19 @@ main = do
       cat <- lift $ findById catId
       entityOr404 (cat :: Maybe (Entity Cat))
 
-    post "/cat" $ undefined
+    post "/cat" $ do
+      cmd <- jsonBody'
+      k <- lift $ updateOrInsert (cmd :: UpdateOrInsert Cat)
+      entityIdOr400 $ Just k -- TODO: temporary hardcoding Just till wrong IDs handled properly
 
     get ("/dog" <//> var) $ \(dogId :: Int64) -> do
       dog <- lift $ findById dogId
       entityOr404 (dog :: Maybe (Entity Dog))
 
-    post "/dog" $ undefined
+    post "/dog" $ do
+      cmd <- jsonBody'
+      k <- lift $ updateOrInsert (cmd :: UpdateOrInsert Dog)
+      entityIdOr400 $ Just k -- TODO: temporary hardcoding Just till wrong IDs handled properly
 
 
 entityOr404 :: (ToJSON ent, Show ent) => Maybe (Entity ent) -> ActionT AppM ()
@@ -40,3 +49,9 @@ entityOr404 maybeEntity =
   case maybeEntity of
     Nothing     -> setStatus status404
     Just entity -> json $ encodeEntity entity
+
+entityIdOr400 :: ToJSON (Key ent) => Maybe (Key ent) -> ActionT AppM ()
+entityIdOr400 maybeId =
+  case maybeId of
+    Nothing       -> setStatus status400
+    Just entityId -> json $ object [ "id" .= entityId]
